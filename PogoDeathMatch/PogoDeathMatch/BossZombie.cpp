84f884a7aed020e73f848d71a4f08aec7cd1defe
Lucas <ldellisola@itba.edu.ar>
2018-09-30 01:54:01 -0300

    hola

--- PogoDeathMatch/PogoDeathMatch/BossZombie.cpp
+++ PogoDeathMatch/PogoDeathMatch/BossZombie.cpp
@@ -2,12 +2,12 @@
 
 
 
-#define BossForce ()
-#define BossMass ()
-#define BossSpeed ()
-#define BossDamp ()
-#define BossSizeCoef ()
-#define BossPoints ()
+#define BossForce (1)
+#define BossMass (1)
+#define BossSpeed (1)
+#define BossDamp (1)
+#define BossSizeCoef (1)
+#define BossPoints (1)
 
 BossZombie::BossZombie(AllegroSound * jump, AllegroSound * death, AllegroSprite * sprite, float x, float y, float radius)
 	:Zombie(jump, death, sprite, x, y, BossForce, BossMass, BossSpeed, radius, BossDamp, BossSizeCoef)
