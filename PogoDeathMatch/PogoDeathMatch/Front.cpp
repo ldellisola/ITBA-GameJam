d0e25e81fdd4e5d52e3b1f3ff46abe1240010573
Lucas <ldellisola@itba.edu.ar>
2018-09-30 05:07:37 -0300

    merge incoming

--- PogoDeathMatch/PogoDeathMatch/Front.cpp
+++ PogoDeathMatch/PogoDeathMatch/Front.cpp
@@ -22,5 +22,55 @@ int Menu::checkForPress(float mouseX, float mouseY, double timeStamp)
 		button = buttonVal::INSTRUCTIONS;
 	}
 
+	if (((AllegroButton*)boxes[CREDITS_ID])->click(mouseX, mouseY, timeStamp)) {
+		button = buttonVal::CREDITS;
+	}
+
 	return button;
+}
+
+void Menu::runCredits(AllegroWindow * window, AllegroEventHandler & eventHandler)
+{
+	window->deleteLayout();
+	window->insertLayout(credits);
+	bool leave = false;
+
+	while (!leave) {
+		eventHandler.getEvent();
+
+		if (eventHandler.isThereEvent()) {
+			AllegroEvent ev = eventHandler.ObtainEvent();
+			if (ev.getType() == EventType::MouseDown) {
+				if (creditBoxes[CredButtonID]->click(ev.getX(), ev.getY(), ev.getTimestamp()))
+					leave = true;
+			}
+			else if (ev.getType() == EventType::Timer)
+				window->update();
+			
+		}
+	}
+	
+}
+
+void Menu::runInstructions(AllegroWindow * window, AllegroEventHandler & eventHandler)
+{
+
+	window->deleteLayout();
+	window->insertLayout(instructions);
+	bool leave = false;
+
+	while (!leave) {
+		eventHandler.getEvent();
+
+		if (eventHandler.isThereEvent()) {
+			AllegroEvent ev = eventHandler.ObtainEvent();
+			if (ev.getType() == EventType::MouseDown) {
+				if (instrucBoxes[InstButtonID]->click(ev.getX(), ev.getY(), ev.getTimestamp()))
+					leave = true;
+			}
+			else if (ev.getType() == EventType::Timer)
+				window->update();
+
+		}
+	}
 }
